2ca81aa5ad4947be389a70d3b2abdf1292a79946
Chamyko <Chamyko12345@gmail.com>
2016-02-09 11:30:12 -0800

    Save changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,18 +7,19 @@ class UsersController < ApplicationController
     @user=User.new
   end
   def register
-    @user=User.new(user_params)
+    @user=User.new(user_params_register)
 
       respond_to do |format|
         if (@user.save)
-          format.html {redirect_to 'welcome/index', notice: 'You may sign in now'}
+          format.html {redirect_to root_path, notice: 'You may sign in now'}
         else
-          format.html {'new'}
+          format.html {render 'new'}
+          #format.json{@user}
       end
     end
   end
   private
-  def user_params
-    params.require(:user).permit(:email, :password)
+  def user_params_register
+    params.require(:user).permit(:email,:password,:password_gonfirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/login' => 'users#login'
-  get 'users/new' => 'users#new'
-  post 'users/register' => 'users#register'
+  resource :users do
+    member do
+      get 'new'
+      post 'register'
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
