b46fd5304a540da09bb267839db7b6fbe6fe6541
Chamyko <Chamyko12345@gmail.com>
2016-02-15 15:34:05 -0800

    Make changes

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,35 @@
+class BooksController < ApplicationController
+  include BooksHelper
+  layout 'application'
+  def index
+    @books=Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book=Book.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show_old
+  end
+
+  def show_new
+  end
+
+  def search
+  end
+end
---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,10 @@
+class CategoriesController < ApplicationController
+  layout 'application'
+  def new
+    #render 'new'
+  end
+
+  def create
+    
+  end
+end
