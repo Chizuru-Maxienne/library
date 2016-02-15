1cb6adb92f9f730484e8d23140f4ca97110b43fd
Chamyko <Chamyko12345@gmail.com>
2016-02-15 14:22:11 -0800

    Make changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215221500) do
+ActiveRecord::Schema.define(version: 20160215222057) do
 
   create_table "add_isbn_unique_to_books", force: :cascade do |t|
   end
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20160215221500) do
   create_table "auths", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "book_types", force: :cascade do |t|
+    t.string   "category",   limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "books", id: false, force: :cascade do |t|
