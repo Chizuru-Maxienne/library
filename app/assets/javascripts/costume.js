23ec7703bdd4b82da027fb13269f95c3e7262b78
Chamyko <Chamyko12345@gmail.com>
2016-02-15 16:33:12 -0800

    Initialized commit

---
+++ app/assets/javascripts/costume.js
@@ -0,0 +1,11 @@
+/**
+ * Created by pc on 2/15/2016.
+ */
+jQuery.noConflict();
+(function($){
+    $(function() {
+        $("model-books").click(function() {
+            $("#library-modal").modal()
+        });
+    });
+})(jQuery);
